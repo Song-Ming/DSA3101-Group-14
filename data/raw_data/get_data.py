50fb21cf621052255f601c3c19094824794486f6
Xin Hui Ong <xinhuiongg@gmail.com>
2025-04-01 16:13:56 +0800

    fix error

--- data/raw_data/get_data.py
+++ data/raw_data/get_data.py
@@ -2,7 +2,6 @@
 import pandas as pd
 import kagglehub
 
-
 path = kagglehub.dataset_download("berkayalan/bank-marketing-data-set")
 df = pd.read_csv(f"{path}/bank_marketing_dataset.csv")
 df.to_csv('./data/raw_data/bank_marketing_dataset.csv', index=False)
